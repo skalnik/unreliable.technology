52a321b706625bb2b38e5eb34268a5360a08da1e
Mike Skalnik <mike.skalnik@gmail.com>
2018-01-25 18:04:50 -0800

    :sunrise:

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) =>
+  if(Math.random() < .05) {
+    res.send('Welcome to Unreliable Technology!')
+  } else {
+    res.status('500').send('Something went wrong!')
+  }
+)
+
+app.listen(port, () => console.log('Listening on port ' + port))
